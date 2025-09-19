988d12c8a8be69760c49b4620df70ece1156f399
Christian Zang <cszang@users.noreply.github.com>
2025-09-19 12:08:16 +0200

    Change ggplot color to red for mean price points

--- analysis.R
+++ analysis.R
@@ -4,5 +4,5 @@ diamonds %>%
   group_by(color) %>% 
   summarise(mean_price = mean(price),
             sd_price = sd(price)) %>% 
-  ggplot(aes(color, mean_price, size = sd_price)) +
+  ggplot(aes(color, mean_price, size = sd_price), colour = "red") +
   geom_point()
