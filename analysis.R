a7b6aba500eb6e092947832527eb3b910b80f85b
Christian Zang <christian.zang@hswt.de>
2025-09-19 11:29:51 +0200

    added a nice plot

--- analysis.R
+++ analysis.R
@@ -3,4 +3,6 @@ library(tidyverse)
 diamonds %>% 
   group_by(color) %>% 
   summarise(mean_price = mean(price),
-            sd_price = sd(price))
+            sd_price = sd(price)) %>% 
+  ggplot(aes(color, mean_price, size = sd_price)) +
+  geom_point()
